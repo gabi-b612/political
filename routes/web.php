8c54d592648c9222a0c2ad33fde4a6fa1913a242
gabi-b612 <20km095@esisalama.org>
2024-07-29 11:53:24 +0200

    feat[clear comment]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CandidatureController;
 use App\Http\Controllers\MembreController;
 use App\Http\Controllers\OffreDemploiController;
 use App\Http\Controllers\UtilisateurController;
@@ -10,16 +9,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
-
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/admin' , function () {
@@ -28,9 +17,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('admin/utilisateurs', UtilisateurController::class);
     Route::resource('admin/membres', MembreController::class);
     Route::resource('admin/offres', OffreDemploiController::class);
-//    Route::resource('candidatures', CandidatureController::class);
-//    Route::get('candidatures/{id}/destroy', [CandidatureController::class, 'destroy'])->name('candidatures.destroy');
-//    Route::delete('candidatures/{id}', [CandidatureController::class, 'destroyConfirmed'])->name('candidatures.destroyConfirmed');
 });
 
 require __DIR__.'/auth.php';
